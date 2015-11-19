eb3ad6f47a05d13b28114e9f3d49da093a5a4da2
Dimitar Valchanov <cerrutito@gmail.com>
2015-11-19 14:03:17 +0200

    Fix indentation

--- server.js
+++ server.js
@@ -3,19 +3,17 @@ var WebpackDevServer = require("webpack-dev-server");
 var config = require("./webpack.config.hot");
 
 new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    watch: true,
-    inline: true,
-    sourceMap: true,
-    stats: {
-        colors: true
-    },
-    historyApiFallback: true
+  publicPath: config.output.publicPath,
+  hot: true,
+  watch: true,
+  inline: true,
+  sourceMap: true,
+  stats: {
+    colors: true
+  },
+  historyApiFallback: true
 }).listen(3000, "0.0.0.0", function(err) {
-    if (err) {
-        console.log(err);
-    }
+  if (err) console.log(err);
 
-    console.log("Listening at 0.0.0.0:3000");
+  console.log("Listening at 0.0.0.0:3000");
 });
